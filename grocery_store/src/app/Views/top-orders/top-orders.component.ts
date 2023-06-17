4460c2e721dd44a2344b6c68464d549cfa22f5b1
Debraj Kundu <debraj.kundu@nagarro.com>
2023-06-17 11:50:22 +0530

    date and number select for top orders

--- grocery_store/src/app/Views/top-orders/top-orders.component.ts
+++ grocery_store/src/app/Views/top-orders/top-orders.component.ts
@@ -1,20 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopOrderService } from 'src/app/Shared/Service/topOrder.service';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule, MatDatepicker } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  MomentDateAdapter,
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+} from '@angular/material-moment-adapter';
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+} from '@angular/material/core';
+
+import * as _moment from 'moment';
+import { Moment } from 'moment';
+
+const moment = _moment;
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'MM/YYYY',
+  },
+  display: {
+    dateInput: 'MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
+
+const matModules = [
+  MatFormFieldModule,
+  MatCardModule,
+  MatButtonModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+];
 
 @Component({
   selector: 'app-top-orders',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, ...matModules],
   templateUrl: './top-orders.component.html',
   styleUrls: ['./top-orders.component.css'],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+  ],
+  encapsulation: ViewEncapsulation.None,
 })
-export class TopOrdersComponent {
-  constructor(private topOrderService: TopOrderService) {}
+export class TopOrdersComponent implements OnInit {
+  constructor(private topOrderService: TopOrderService, private fb: FormBuilder) {}
+  
+  ngOnInit(): void {
+    this.number = this.fb.group({
+      number: new FormControl(5)
+    })
+  }
 
   topOrders$ = this.topOrderService.getTopOrderedProducts();
 
-  getByDate(){//get from a dropdown
-    this.topOrders$ = this.topOrderService.getTopOrderedProducts(5, 5, 0);
+  date = new FormControl(moment());
+  number!: FormGroup;
+  
+  setMonthAndYear(
+    normalizedMonthAndYear: Moment,
+    datepicker: MatDatepicker<Moment>
+  ) {
+    const ctrlValue = this.date.value!;
+    ctrlValue.month(normalizedMonthAndYear.month());
+    ctrlValue.year(normalizedMonthAndYear.year());
+    this.date.setValue(ctrlValue);
+    datepicker.close();
+    
+    let month:number = this.date.value?.month()??0;
+    let year:number = this.date.value?.year()??0;
+    month = month+1;
+    this.topOrders$ = this.topOrderService.getTopOrderedProducts(this.number.value.number, month, year);
   }
+
 }
