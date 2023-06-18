d1e1998910f83a67e2128c724ff563f7d2ec446d
Debraj Kundu <debraj.kundu@nagarro.com>
2023-06-18 19:37:52 +0530

    side nav

--- grocery_store/src/app/Views/cart/cart.component.ts
+++ grocery_store/src/app/Views/cart/cart.component.ts
@@ -7,18 +7,29 @@ import { Cart } from 'src/app/Shared/Interface/Cart.interface';
 import { Observable } from 'rxjs';
 import { OrderService } from 'src/app/Shared/Service/order.service';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { ToastService } from 'src/app/Shared/Service/toast.service';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+  ],
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
+  imageBaseUrl = 'https://localhost:44333/resources/';
+
   constructor(
     private cartService: CartService,
-    private orderService: OrderService
+    private orderService: OrderService,
+    private toast: ToastService
   ) {}
 
   selectedProdId: number = 0;
@@ -46,8 +57,15 @@ export class CartComponent {
   }
 
   deleteCart(id: number) {
-    this.cartService.deleteCartItem(id).subscribe((res) => {
-      this.cartList$ = this.cartService.getCart();
+    confirm('You are about to delete the product');
+    this.cartService.deleteCartItem(id).subscribe({
+      next: (res) => {
+        this.cartList$ = this.cartService.getCart();
+        this.toast.successToast('Product deleted successfully!');
+      },
+      error: (err) => {
+        this.toast.errorToast('Unexpected error occured');
+      },
     });
   }
 }
