6b95459e013edb4f4f47091fe1c45716690a1534
Debraj Kundu <debraj.kundu@nagarro.com>
2023-06-18 22:51:30 +0530

    Logging added

--- DebrajKundu_3203000_FinalTest/Program.cs
+++ DebrajKundu_3203000_FinalTest/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,7 @@ namespace DebrajKundu_3203000_FinalTest
     {
         public static void Main(string[] args)
         {
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -21,6 +23,11 @@ namespace DebrajKundu_3203000_FinalTest
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                }).ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
+      .UseNLog();
     }
 }
